5cd42b3741dfe6dcb761f28f10e26f80cab11c74
Poman <ayujolin520@gmail.com>
2019-01-10 00:40:33 +0800

    homework 2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -3,8 +3,36 @@ package com.train;
 import java.util.Scanner;
 
 public class Tester {
+    private static boolean isExit = false;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+//        homework1(scanner);
+        homework2(scanner);
+    }
+
+    private static void homework2(Scanner scanner) {
+        do {
+            System.out.print("Please enter number of tickets or enter -1 to end: ");
+            int tickets = scanner.nextInt();
+            if (tickets == -1) {
+                isExit = true;
+                break;
+            }
+            System.out.print("How many round-trip tickets or enter -1 to end: ");
+            int roundTrip = scanner.nextInt();
+            if (roundTrip == -1) {
+                isExit = true;
+                break;
+            }
+            Ticket ticket = new Ticket(tickets, roundTrip);
+            ticket.fees();
+        } while(!isExit);
+        System.out.println("Thank you!");
+    }
+
+    private static void homework1(Scanner scanner) {
         System.out.print("Please enter number of tickets: ");
         int tickets = scanner.nextInt();
         System.out.print("How many round-trip tickets: ");
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -2,8 +2,35 @@ package com.train
 
 import java.util.*
 
+var isExit: Boolean = false
+
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
+//    homework1(scanner)
+    homework2(scanner)
+}
+
+fun homework2(scanner: Scanner) {
+    do {
+        print("Please enter number of tickets or enter -1 to end: ")
+        val tickets = scanner.nextInt()
+        if (tickets == -1) {
+            isExit = true
+            break
+        }
+        print("How many round-trip tickets or enter -1 to end: ")
+        val roundTrip = scanner.nextInt()
+        if (roundTrip == -1) {
+            isExit = true
+            break
+        }
+        val ticket = TicketKotlin(tickets, roundTrip)
+        ticket.fees()
+    } while (!isExit)
+    println("Thank you!")
+}
+
+private fun homework1(scanner: Scanner) {
     print("Please enter number of tickets: ")
     val tickets = scanner.nextInt()
     print("How many round-trip tickets: ")
